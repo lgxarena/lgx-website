cbad967c191bd16174e0523cbac74e9cc91de4b8
Diego Gomes <diegosgomes27@gmail.com>
2025-01-09 23:34:02 -0300

    feat: adding home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
-    <div className="h-[200vh] bg-primary-400">
-      <h1>Hello World!</h1>
+    <div>
+      <section
+        className="flex h-[calc(100dvh-72px)] w-full flex-col items-center justify-center gap-10 bg-hero-mobile
+          bg-cover bg-center bg-no-repeat px-5"
+      >
+        <h1 className="text-center text-4xl font-bold [text-shadow:0_0_2px_#ffffff]">
+          ONDE OS <span className="block sm:inline">GAMERS</span>&nbsp;
+          <span>SE TORNAM LENDAS</span>
+        </h1>
+        <Button className="w-full">Faça sua reserva</Button>
+      </section>
     </div>
   )
 }
